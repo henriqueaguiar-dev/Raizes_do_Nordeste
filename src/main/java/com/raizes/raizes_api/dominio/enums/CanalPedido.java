a4a147c43f6c4f9f1d73f14114d25671aff79df0
Henrique Aguiar <rickdf7@gmail.com>
2026-09-11 10:48:36 -0400

    Add StatusPagamento enum for payment states

--- src/main/java/com/raizes/raizes_api/dominio/enums/CanalPedido.java
+++ src/main/java/com/raizes/raizes_api/dominio/enums/CanalPedido.java
@@ -1,5 +1,9 @@
 package com.raizes.raizes_api.dominio.enums;
 
 public enum CanalPedido {
-
+    APP,
+    TOTEM,
+    BALCAO,
+    PICKUP,
+    WEB
 }
--- src/main/java/com/raizes/raizes_api/dominio/enums/StatusPedido.java
+++ src/main/java/com/raizes/raizes_api/dominio/enums/StatusPedido.java
@@ -1,5 +1,11 @@
 package com.raizes.raizes_api.dominio.enums;
 
 public enum StatusPedido {
+    AGUARDANDO_PAGAMENTO,
+    PAGO,
+    EM_PREPARO,
+    PRONTO,
+    ENTREGUE,
+    CANCELADO
 
 }
