6bba449c64564d94a4ab6a4bde2f87abed8a20cf
Henrique Aguiar <rickdf7@gmail.com>
2026-09-16 16:25:57 -0400

    Add JPA repository for Produto

--- src/main/java/com/raizes/raizes_api/infraestrutura/persistencia/repositorio/ProdutoJpaRepositorio.java
+++ src/main/java/com/raizes/raizes_api/infraestrutura/persistencia/repositorio/ProdutoJpaRepositorio.java
@@ -1,5 +1,11 @@
 package com.raizes.raizes_api.infraestrutura.persistencia.repositorio;
 
-public interface ProdutoJpaRepositorio {
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.raizes.raizes_api.dominio.modelo.Produto;
+
+public interface ProdutoJpaRepositorio extends JpaRepository<Produto, UUID> {
 
 }
