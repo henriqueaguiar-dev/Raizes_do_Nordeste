dc102bf294cd7b35411752e8927e02a50a11079e
Henrique Aguiar <rickdf7@gmail.com>
2026-09-16 17:12:36 -0400

    Add JPA repository for Pedido

--- src/main/java/com/raizes/raizes_api/infraestrutura/persistencia/repositorio/EstoqueJpaRepositorio.java
+++ src/main/java/com/raizes/raizes_api/infraestrutura/persistencia/repositorio/EstoqueJpaRepositorio.java
@@ -1,9 +1,13 @@
 package com.raizes.raizes_api.infraestrutura.persistencia.repositorio;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.raizes.raizes_api.infraestrutura.persistencia.entidade.EstoqueEntidade;
 
 public interface EstoqueJpaRepositorio extends JpaRepository<EstoqueEntidade, UUID> {
 
+    Optional<EstoqueEntidade> findByProdutoId(UUID unidadeId, UUID produtoId);
 }
---
+++ src/main/java/com/raizes/raizes_api/infraestrutura/persistencia/repositorio/PedidoJpaRepositorio.java
@@ -0,0 +1,20 @@
+package com.raizes.raizes_api.infraestrutura.persistencia.repositorio;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.raizes.raizes_api.dominio.enums.CanalPedido;
+import com.raizes.raizes_api.dominio.enums.StatusPedido;
+import com.raizes.raizes_api.infraestrutura.persistencia.entidade.PedidoEntidade;
+
+public interface PedidoJpaRepositorio extends JpaRepository<PedidoEntidade, UUID> {
+
+    List<PedidoEntidade> findByClienteId(UUID clienteId);
+
+    List<PedidoEntidade> FindByCanlPedido(CanalPedido canalPedido);
+
+    List<PedidoEntidade> findByStatus(StatusPedido statusPedido);
+
+}
